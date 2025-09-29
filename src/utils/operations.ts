61d27230219eb747ec7182d45ff0fe5284435e22
Aryan Kumar <aryanak9163@gmail.com>
2025-09-29 11:47:02 +0530

    moved to reusable function from custom route

--- src/utils/operations.ts
+++ src/utils/operations.ts
@@ -1,19 +1,28 @@
-import { bucketName, r2 } from "@/lib/r2";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import {bucketName, r2} from "@/lib/r2";
+import {DeleteObjectCommand, PutObjectCommand} from "@aws-sdk/client-s3";
 
 interface FileUpload {
-  key: string;
-  buffer: Buffer;
-  contentType: string;
+    key: string;
+    buffer: Buffer;
+    contentType: string;
 }
 
-export const fileUpload = async ({ key, buffer, contentType }: FileUpload) => {
-  await r2.send(
-    new PutObjectCommand({
-      Bucket: bucketName,
-      Key: key,
-      Body: buffer,
-      ContentType: contentType,
-    }),
-  );
+export const fileUpload = async ({key, buffer, contentType}: FileUpload) => {
+    await r2.send(
+        new PutObjectCommand({
+            Bucket: bucketName,
+            Key: key,
+            Body: buffer,
+            ContentType: contentType,
+        }),
+    );
 };
+
+export const fileDelete = async ({key}: { key: string }) => {
+    await r2.send(
+        new DeleteObjectCommand({
+            Bucket: bucketName,
+            Key: key,
+        }),
+    )
+}
