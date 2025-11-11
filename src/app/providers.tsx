df4f97a5e84b3d9c90983c32218edfdae98ae1b0
Aryan Kumar <aryanak9163@gmail.com>
2025-11-12 01:49:56 +0530

    updates the providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,22 +2,33 @@
 import * as React from "react";
 import {ThemeProvider as NextThemeProvider} from "next-themes";
 import {type ThemeProviderProps} from "next-themes";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {TooltipProvider} from "@radix-ui/react-tooltip";
+import {Toaster as Sonner} from "@/components/ui/sonner"
+
 
 interface ProviderProps extends ThemeProviderProps {
     children: React.ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 export default function Providers({children, ...props}: ProviderProps) {
     return (
-        <NextThemeProvider
-            attribute={"class"}
-            defaultTheme={"system"}
-            enableSystem
-            disableTransitionOnChange
-            {...props}
-        >
-            {children}
-        </NextThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <NextThemeProvider
+                attribute={"class"}
+                defaultTheme={"system"}
+                enableSystem
+                disableTransitionOnChange
+                {...props}
+            >
+                <TooltipProvider>
+                    <Sonner/>
+                    {children}
+                </TooltipProvider>
+            </NextThemeProvider>
+        </QueryClientProvider>
     )
 
 }
