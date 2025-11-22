d5a4d1f7200310f69734ff179532f552118f5f4f
Aryan Kumar <aryanak9163@gmail.com>
2025-11-23 04:50:33 +0530

    fix: remove useless files and update workspace configuration

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import path from "node:path";
+import {defineConfig} from "prisma/config"
+
+export default defineConfig({
+    schema: path.join(process.cwd(), "prisma", "schema.prisma"),
+    migrations: {
+        path: path.join(process.cwd(), "prisma", "migrations"),
+        seed: "pnpm exec tsx prisma/seed.ts"
+    }
+})
