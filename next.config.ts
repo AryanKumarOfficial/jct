d6436db70adeb6a106ff2ee1eb0b0dbfec0ab304
Aryan Kumar <aryanak9163@gmail.com>
2025-12-14 03:14:52 +0530

    renames `api-docs` route for better structure, cleans up unused image configuration in `next.config.ts`, and adjusts workspace settings

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        unoptimized: true,
-    },
-    output: "standalone"
+    output: "standalone",
 };
 
 export default nextConfig;
